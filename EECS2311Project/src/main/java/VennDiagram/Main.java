2d92c647f0213eb115a0a60938d4959e8cc62f30
brianmw1 <brianmw1@my.yorku.ca>
2020-01-29 13:57:51 -0500

    changed main

--- EECS2311Project/src/main/java/VennDiagram/Main.java
+++ EECS2311Project/src/main/java/VennDiagram/Main.java
@@ -3,7 +3,7 @@ package VennDiagram;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world.");
+		System.out.println("Hello world. Change by Brian");
 
 	}
 
