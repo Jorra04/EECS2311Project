ffd08fd05dd8e89e3a43477783427810d7d20aff
jorra04 <jorra04@my.yorku.ca>
2020-02-05 14:50:17 -0500

    scenebuilder

--- EECS2311Project/src/main/java/VennDiagram/Main.java
+++ EECS2311Project/src/main/java/VennDiagram/Main.java
@@ -20,71 +20,26 @@ import javafx.scene.shape.Circle;
 import javafx.scene.Group;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
 
+public class Main extends Application {
 
-public class Main extends Application{
-	
-	//Button button;
+	// Button button;
 	model.Item item;
+	@FXML
+	private Circle circle1;
+
+	@FXML
+	private Circle circle2;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	@Override
-	public void start(Stage primaryStage) throws Exception{
-		//brian change
-		primaryStage.setTitle("Venn Diagram");
-	
-		Circle circle1 = new Circle();
-		circle1.setCenterX(250.0f);
-		circle1.setCenterY(135.0f);
-		circle1.setRadius(100.0f);
-		circle1.setStroke(Color.GREEN);
-		circle1.setFill(Color.rgb(200, 200, 200, 0.5));
+	public void start(Stage primaryStage) throws Exception {
 		
-		Circle circle2 = new Circle();
-		circle2.setCenterX(350.0f);
-		circle2.setCenterY(135.0f);
-		circle2.setRadius(100.0f);
-		circle2.setStroke(Color.BLUE);
-		circle2.setFill(Color.rgb(255, 0, 0, 0.2));
-//		
-		
-//		adds button to Venn Diagram Application 
-
-        Button button = new Button ("Click me");
-        
-        TextField textField = new TextField ("Enter");
-        textField.setPrefHeight(20);
-        textField.setPrefWidth(6* textField.getPrefHeight());
-        textField.setLayoutX(0);
-        textField.setLayoutY(30);
-        button.setLayoutY(textField.getLayoutY() + 1.25*textField.getPrefHeight());
-        button.setLayoutX(textField.getLayoutX()); 
-        button.setBackground(Background.EMPTY);
-       
-        //StackPane r = new StackPane(); 
-		
-        
-        
-		Group root = new Group();
-		
-		root.getChildren().add(circle1);
-		root.getChildren().add(circle2);
-	
-		root.getChildren().add(textField);
-		root.getChildren().add(button);
-		
-	
-
-		
-		Scene scene = new Scene(root, 600, 300);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		//comment
-
 	}
-	
-}
 
+}
