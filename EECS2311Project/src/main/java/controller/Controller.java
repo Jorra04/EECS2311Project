88386d913681cdb06edc74179fa7e253eaa31b75
brianmw1 <brianmw1@my.yorku.ca>
2020-02-06 09:21:22 -0500

    fixed bug where fxml addition broke project

--- EECS2311Project/src/main/java/controller/Controller.java
+++ EECS2311Project/src/main/java/controller/Controller.java
@@ -1,20 +1,19 @@
 package controller;
 
 import model.VennModel;
-import VennDiagram.Main;
+import VennDiagram.View;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 
 public class Controller {
 	VennModel model;
 	//view here
-	Main main;
-	public Controller(VennModel model, Main main) {
+	View view;
+	public Controller(VennModel model, View view) {
 		this.model = model;
-		this.main = main;
+		this.view = view;
 	}
-	private void handleButtonAction(ActionEvent event)
-    {
+	private void handleButtonAction(ActionEvent event) {
         Button button = (Button)event.getSource();
     }
 	
