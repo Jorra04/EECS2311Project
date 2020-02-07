4e58325b71ad4fb6bb207beb7fd28366325b63b8
jorra04 <jorra04@my.yorku.ca>
2020-02-07 01:30:32 -0500

    added/modified functionality.

--- EECS2311Project/src/main/java/controller/Controller.java
+++ EECS2311Project/src/main/java/controller/Controller.java
@@ -1,4 +1,5 @@
 package controller;
+
 import java.util.*;
 
 import javafx.collections.FXCollections;
@@ -8,34 +9,58 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import model.VennModel;
 import model.Item;
- 
+
 public class Controller {
-	
-	//create venn diagram instance
-    private VennModel model = new VennModel();
-    
-    ObservableList<String> content = FXCollections.observableArrayList();
-	
-	//fxml components
-	@FXML TextField create_text;
-	@FXML Button create_button;
-	@FXML ListView<String> item_list;
-	@FXML Pane diagram_pane; //venn diagram is here
-    @FXML Button clearButton; //trying to use this clear button
-    @FXML 
-    protected void handleCreateButtonAction(ActionEvent event) {
-        Item item = new Item(create_text.getText());
-        content.add(item.toString());
-       
-        System.out.println(content);
-        item_list.setItems(content);
-    }
-    @FXML
-    protected void handleClearButtonAction(ActionEvent event) {
-    	item_list.getItems().clear();
-    }
+
+	// create venn diagram instance
+	private VennModel model = new VennModel();
+
+	ObservableList<String> content = FXCollections.observableArrayList();
+
+	// fxml components
+	@FXML
+	TextField create_text;
+	@FXML
+	Button create_button;
+	@FXML
+	ListView<String> item_list;
+	@FXML
+	Pane diagram_pane; // venn diagram is here
+	@FXML
+	Button clearButton; // trying to use this clear button
+
+	@FXML
+
+	protected void handleCreateButtonAction(ActionEvent event){
+		if (create_text.getLength() != 0) {
+			Item item = new Item(create_text.getText());
+			content.add(item.toString());
+
+			//System.out.println(content);
+			item_list.setItems(content);
+			
+			create_text.clear();
+			create_text.requestFocus();
+			
+		
+		}
+
+	}
+
+	@FXML
+	protected void handleClearAllButtonAction(ActionEvent event) {
+		item_list.getItems().clear();
+	}
+
+	@FXML
+	protected void handleClearSelectedButtonAction(ActionEvent event) {
+		List<String> copyList = new ArrayList<>(item_list.getSelectionModel().getSelectedItems());
+		item_list.getItems().removeAll(copyList);
+
+	}
 
 }
