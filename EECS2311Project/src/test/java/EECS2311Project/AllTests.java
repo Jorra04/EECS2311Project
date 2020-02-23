aef97113f2707c4ec3053883c16840fbb75ff410
brianmw1 <brianmw1@My.yorku.ca>
2020-02-23 18:42:09 -0500

    added documentation folder, implemented grouptest, changes to group class

--- EECS2311Project/src/test/java/EECS2311Project/AllTests.java
+++ EECS2311Project/src/test/java/EECS2311Project/AllTests.java
@@ -4,10 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import guitest.GUITest;
+import modeltest.GroupTest;
+import modeltest.ItemTest;
 import modeltest.ModelTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({GUITest.class, ModelTest.class})
+@SuiteClasses({GUITest.class, ModelTest.class, GroupTest.class, ItemTest.class})
 public class AllTests {
 
 }
--- EECS2311Project/src/test/java/modeltest/GroupTest.java
+++ EECS2311Project/src/test/java/modeltest/GroupTest.java
@@ -5,11 +5,14 @@ package modeltest;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import model.Group;
+import model.Item;
 
 /**
  * @author Brian
@@ -32,11 +35,81 @@ public class GroupTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		Item.resetUID();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInsertItem() {
+		Item item = new Item("apple");
+		group.insertItem(item);
+		assertEquals(true, group.getItem(item.getID()).equals(item));
 	}
-
+	
+	@Test
+	public void testInsertItems() {
+		ArrayList<Item> items = new ArrayList<Item>();
+		Item item1 = new Item("apple");
+		Item item2 = new Item("mango");
+		items.add(item1);
+		items.add(item2);
+		group.insertItems(items);
+		assertEquals(true, group.getItem(1).equals(item1));
+		assertEquals(true, group.getItem(2).equals(item2));
+	}
+	
+	@Test
+	public void testRemoveItem() {
+		Item item1 = new Item("apple");
+		group.insertItem(item1);
+		group.removeItem(item1);
+		assertEquals(null, group.getItem(item1.getID()));
+	}
+	
+	@Test
+	public void testRemoveItems() {
+		ArrayList<Item> items = new ArrayList<Item>();
+		Item item1 = new Item("apple");
+		Item item2 = new Item("mango");
+		items.add(item1);
+		items.add(item2);
+		group.insertItems(items);
+		group.removeItems(items);
+		assertEquals(0, group.getSize());
+	}
+	
+	@Test
+	public void testRemoveAll() {
+		ArrayList<Item> items = new ArrayList<Item>();
+		Item item1 = new Item("apple");
+		Item item2 = new Item("mango");
+		items.add(item1);
+		items.add(item2);
+		group.insertItems(items);
+		group.removeAll();
+		assertEquals(0, group.getSize());
+	}
+	
+	@Test
+	public void testGetTitle() {
+		assertEquals("test", group.getTitle());
+	}
+	
+	@Test
+	public void testSetTitle() {
+		group.setTitle("pineapple on pizza");
+		assertEquals("pineapple on pizza", group.getTitle());
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		//negative test
+		group.insertItem(new Item("steak"));
+		assertEquals(false, group.isEmpty());
+	}
+	
+	@Test
+	public void testFindMatching() {
+		
+	}
+	
 }
