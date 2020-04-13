74d5cb767b42d9f977403561e3742c2ac852a90f
gisone-bartolomeo-213704291 <bgisone@my.yorku.ca>
2020-04-13 16:07:12 -0400

    100% group test coverage.

--- EECS2311Project/src/test/java/modeltest/GroupTest.java
+++ EECS2311Project/src/test/java/modeltest/GroupTest.java
@@ -6,8 +6,11 @@ package modeltest;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.TreeMap;
 import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.After;
 import org.junit.Before;
@@ -130,11 +133,6 @@ public class GroupTest {
 		group.removeAll();
 		assertEquals(0, group.getSize());
 	}
-	
-	@Test
-	public void testFindMatching() {
-		
-	}
 	@Test
 	public void testGetItems() {
 		Item item1 = new Item("item1");
@@ -147,9 +145,67 @@ public class GroupTest {
 	
 	@Test
 	public void testContains() {
+	
+		ArrayList<Item> items = new ArrayList<Item>();
+		Item item1 = new Item("apple");
+		Item item2 = new Item("mango");
+		Item item3 = new Item("banana");
+		items.add(item1);
+		items.add(item2);
+		group.insertItems(items);
+		
+		
+		assertEquals(true, group.contains(item2));
+		assertEquals(false, group.contains(item3));	
+	}
+	@Test
+	public void testToVisualList() {
+		ArrayList<Item> items = new ArrayList<Item>();
+		Item item1 = new Item("apple");
+		Item item2 = new Item("mango");
+		items.add(item1);
+		items.add(item2);
+		group.insertItems(items);
+		
+		assertEquals(group.toVisualList(), "apple" + "\n" + "mango"+ "\n");		
 		
 	}
-	
+	@Test
+	public void testToSet() {
+		ArrayList<Item> items = new ArrayList<Item>();
+		Item item1 = new Item("apple");
+		Item item2 = new Item("mango");
+		items.add(item1);
+		items.add(item2);
+		group.insertItems(items);
+		
+		String arr [] = {"apple", "mango"};
+		Set<String > a = new HashSet<String>(Arrays.asList(arr)); 
+		
+		
+		assertEquals(true, group.toSet().equals(a));
+		
+	}
+	@Test
+	public void testFindMatching() {
+		ArrayList<Item> items = new ArrayList<Item>();
+		Item item1 = new Item("apple");
+		Item item2 = new Item("mango");
+		Item item3 = new Item("banana");
+		items.add(item1);
+		items.add(item2);
+		group.insertItems(items);
+		
+		Group group2 = new Group("test2");
+		items.add(item3);
+		group2.insertItems(items);
+		
+		
+		Integer arr [] = {item1.getID(), item2.getID()};
+		Set<Integer > a = new HashSet<Integer>(Arrays.asList(arr)); 
+		
+		assertEquals(true, group.findMatching(group2).equals(a));
+	}
 	
 	
 }
